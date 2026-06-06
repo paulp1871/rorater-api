8b82bc0503c6b71a93c6a115fc6ce8a79d0aea2b
Paul Pham <paulpham1871@gmail.com>
2026-06-06 17:18:23 -0600

    Added cors to app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-import type { Application, Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
+import cors, { CorsOptions } from 'cors'
 import { robloxRouter } from './routes/roblox.routes'
 
 const app: Application = express()
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response<{ message: string }>): void => {
